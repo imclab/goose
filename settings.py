bd18ae4c7346ebc481eee5648f38ece805c603dc
Peter Darche <pdarche@gmail.com>
2013-11-28 11:05:03 -0500

    Remove stuff that shouldn't have been there

--- settings.py
+++ settings.py
@@ -12,11 +12,8 @@ define("debug", default=False, help="debug mode")
 settings = {}
 
 settings["debug"] = True
-settings["cookie_secret"] = "askdfjpo83q47r9haskldfjh8"
+settings["cookie_secret"] = "COOKIE SECRET"
 settings["login_url"] = "/login"
 settings["static_path"] = os.path.join(os.path.dirname(__file__), "static")
 settings["template_path"] = os.path.join(os.path.dirname(__file__), "templates")
 settings["xsrf_cookies"] = False
-
-settings['aws_access_key_id'] = 'AKIAJVUP27XRFLHEL3BA'
-settings['aws_secret_access_key'] = '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'
